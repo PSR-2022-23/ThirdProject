9237f8840e5bb215d89f44de61b76dd7954c6cd9
PauloGSP <paulogspereira@ua.pt>
2023-02-06 02:32:14 +0000

    done with teleop (keyboard) updated script, now i know how to run python scripts yey!

--- Scripts/runit.sh
+++ Scripts/runit.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-gnome-terminal -e "bash -c 'cd ~/catkin_ws; source devel/setup.bash; catkin_make install && catkin_make; roslaunch robutler_bringup bringup.launch; exec bash'" &
-gnome-terminal -e "bash -c 'cd ~/catkin_ws; source devel/setup.bash; roslaunch robutler_bringup gazebo.launch; exec bash'"
-
+gnome-terminal -e "bash -c 'cd ~/catkin_ws; source devel/setup.bash; catkin_make install && catkin_make; roslaunch robutler_bringup gazebo.launch; exec bash'" &
+gnome-terminal -e "bash -c 'cd ~/catkin_ws/src/teleop/src/; pwd; chmod +x key_teleop.py; cd ~/catkin_ws; source devel/setup.bash; catkin_make install && catkin_make; roslaunch robutler_bringup bringup.launch; exec bash'" 
